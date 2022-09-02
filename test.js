8dc98c1ff18bf11ee20f831b1ddc6d23d55a0608
Oscar Bernal <oscarbernal93@gmail.com>
2022-09-02 17:34:41 -0500

    More Items Added

--- test.js
+++ test.js
@@ -5,13 +5,58 @@ const material = require("./src/material");
 const render = require("./src/render");
 
 //in explosive and poison, thickness is the concentration
-let obj = create({
-	material:'NU',
+let hasbon = create({
+	material:'LA',
 	quality:0.7,
-	class: 'explosivo',
-	dimension: 0.25,
-	type: 'detonante',
+	class: 'arma',
+	dimension: 0.5,
+	type: 'de_hoja',
+	thickness: 1
+})
+
+let julio = create({
+	material:'LA',
+	quality:1,
+	class: 'arma',
+	dimension: 0.5,
+	type: 'contundente',
 	thickness: 3
+})
+
+let david = create({
+	material: 'FE',
+	quality: 0.7,
+	class: 'arma',
+	dimension: 0.5,
+	type: 'de_tension',
+	thickness: 2
+})
+
+let david2 = create({
+  material: 'AZ',
+  quality: 0.5,
+  class: 'arma',
+  dimension: 0.25,
+  type: 'proyectil',
+  thickness: 1
+})
+
+let david3 = create({
+  material: 'FE',
+  quality: 0.7,
+  class: 'arma',
+  dimension: 0.25,
+  type: 'proyectil',
+  thickness: 1
+})
+
+let martillo = create({
+  material: 'AC',
+  quality: 0.7,
+  class: 'arma',
+  dimension: 0.5,
+  type: 'contundente',
+  thickness: 3 
 })
 
 async function createCards() {
@@ -24,8 +69,8 @@ async function createCards() {
 
 //render.periodic_table()
 
-createCards()
-
+//createCards()
+obj = martillo
 console.log(obj)
 
 //console.log(util.randomObject('weapon'))
